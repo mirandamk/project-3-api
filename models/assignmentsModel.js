53d18f7177b6aff7d679b3733fa2435020903de5
miranda <miranda@LAPTOP-R0JVELH6.localdomain>
2020-04-15 15:28:36 +0200

    added dimensions to model and form

--- models/assignmentsModel.js
+++ models/assignmentsModel.js
@@ -4,7 +4,7 @@ const Schema = mongoose.Schema;
 const assignmentsSchema = new Schema({
  dimension:{
   type: String,
-  enum: ["masculinity", "individualism"],
+  enum: ["power distance", "individualism-collectivism", "masculinity-feminity", "uncertainty avoidance","long term - short term orientation", "indulgence - restraint" ],
  },
  description: {
    type: String,
